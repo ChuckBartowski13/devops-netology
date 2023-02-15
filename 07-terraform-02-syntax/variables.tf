c0f35392f6214832db7fde1e0403a8751d75c552
Churilov Aleksandr <black_ret@mail.ru>
2023-02-15 20:23:21 +0300

    Домашнее задание к занятию "2. Облачные провайдеры и синтаксис Terraform."

---
+++ 07-terraform-02-syntax/variables.tf
@@ -0,0 +1,16 @@
+variable "yandex_cloud_id" {
+  default = "aaaaaa"
+}
+
+variable "yandex_folder_id" {
+  default = "bbbbbb"
+}
+
+variable "centos-7-base" {
+  default = "cccccc"
+}
+
+variable "subnet" {
+  type = string
+  default = "nnnnnnnn"
+}
