326e01373ca845f876c7c1e3148b9b5abe5edd9d
Churilov Aleksandr <black_ret@mail.ru>
2024-05-19 23:08:46 +0300

    clopro

---
+++ 15.2/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "yc_cloud_id" {
+  default = "xxx"
+}
+
+variable "yc_folder_id" {
+  default = "xxx"
+}
+
+variable "yc_token" {
+  type = string
+  default = "xxx"
+}	
+
+variable "yc_region" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default zone for resources"
+}
+
+variable "yc_image" {
+  type        = string
+  default     = "image.png"
+}
+
