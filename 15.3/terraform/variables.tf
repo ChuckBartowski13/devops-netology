9dbbe2a8f7f032ecb0acef4632e79c7044902e27
Churilov Aleksandr <black_ret@mail.ru>
2024-05-21 00:23:17 +0300

    clopro 15.3

---
+++ 15.3/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "yandex_cloud_id" {
+  default = "xxx"
+}
+
+variable "yandex_folder_id" {
+  default = "xxx"
+}
+
+variable "token" {
+  type = string
+  default = "xxx
+}	
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default zone for resources"
+}
